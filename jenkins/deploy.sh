2a95188c9401b258ce52091913fc235535ead7dd
Eduardo Camargo <ecamargo@daitangroup.com>
2017-10-20 13:19:39 -0200

    Bypass host check

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -8,4 +8,4 @@ virtualenv --system-site-packages venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-ansible-playbook -i inventory/ec2.py -e environment_name=${ENVIRONMENT_NAME} playbooks/api.yml
+ansible-playbook -i inventory/ec2.py -e 'host_key_checking=False' -e environment_name=${ENVIRONMENT_NAME} playbooks/api.yml
