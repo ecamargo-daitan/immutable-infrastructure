c627d174794ab96fbb5a3448d00c956577946902
Eduardo Camargo <ecamargo@daitangroup.com>
2017-10-23 16:18:45 -0200

    Using env var to disable ssh fingerprint checks

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -8,4 +8,5 @@ virtualenv --system-site-packages venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-ansible-playbook -i inventory/ec2.py -e 'host_key_checking=False' -e environment_name=${ENVIRONMENT_NAME} playbooks/api.yml
+export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook -i inventory/ec2.py -e environment_name=${ENVIRONMENT_NAME} playbooks/api.yml
